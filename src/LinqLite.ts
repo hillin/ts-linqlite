8128778c69fa774ebd1f54fbd0bee1348efd9ee7
hillin <hillinsilence@gmail.com>
2017-03-27 18:40:32 +0800

    add range() and repeat() methods to ISequence

--- src/LinqLite.ts
+++ src/LinqLite.ts
@@ -1812,6 +1812,29 @@ class Sequence<T> implements ISequence<T> {
     }
 }
 
+// ReSharper disable once InconsistentNaming
+export module ISequence {
+    /**
+ * Generates a sequence of integral numbers within a specified range.
+ * @param start The value of the first integer in the sequence.
+ * @param count The number of sequential integers to generate.
+ * @return An ISequence<Int32> that contains a range of sequential integral numbers.
+ */
+    export function range(start: number, count: number): ISequence<number> {
+        return new Sequence<number>(range(start, count));
+    }
+
+    /**
+     * Generates a sequence that contains one repeated value.
+     * @param element The value to be repeated.
+     * @param count The number of times to repeat the value in the generated sequence.
+     * @return An ISequence<T> that contains a repeated value.
+     */
+    export function repeat<T>(element: T, count: number): ISequence<T> {
+        return new Sequence<T>(repeat(element, count));
+    }
+}
+
 /**
  * Represents a sequence of objects that have a common key.
  */
